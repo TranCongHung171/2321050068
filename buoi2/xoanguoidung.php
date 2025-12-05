a2bed1532dd14f755821d4f5fceba9709658aafb
TranCongHung171 <tranhungcong456@gmail.com>
2025-12-05 11:34:29 +0700

    update php

--- buoi2/xoanguoidung.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("connect.php");
-$id = $_GET['id']; 
-$sql = "DELETE FROM nguoi_dung WHERE id = $id";
-mysqli_query($conn, $sql);
-header("Location: tong.php?page_layout=nguoidung");
-?>
--- buoi2/xoaphim.php
+++
@@ -1,7 +0,0 @@
-<?php
-include("connect.php");
-$id = $_GET['id']; 
-$sql = "DELETE FROM phim WHERE id = $id";
-mysqli_query($conn, $sql);
-header("Location: tong.php?page_layout=phim");
-?>
