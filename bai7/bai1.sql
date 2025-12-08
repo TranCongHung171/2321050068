37ece8c66a212716501e918006eecf803cf89a4d
TranCongHung171 <tranhungcong456@gmail.com>
2025-12-08 13:07:01 +0700

    up

--- bai7/bai1.sql
+++ bai7/bai1.sql
@@ -1,4 +1,4 @@
-# tạo csdl quản lý xem film 
+/*# tạo csdl quản lý xem film 
 CREATE DATABASE IF NOT EXISTS fptplay;
 USE fptplay;
 #SELECT * FROM USER
@@ -63,4 +63,81 @@ CREATE TABLE tapphim(
     trailer VARCHAR
 );
 # btvn: xây dựng 1 csdl qly film: tạo bảng, viết câu lệnh chyaj nhiều lần k bị lỗi
-#mỗi bảng tạo 30dl
+#mỗi bảng tạo 30dl */
+
+CREATE DATABASE IF NOT EXISTS film_fptplay;
+USE film_fptplay;
+-- 1. Bảng thể loại
+CREATE TABLE IF NOT EXISTS the_loai (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    ten_the_loai VARCHAR(50) NOT NULL
+);
+
+-- 2. Bảng vai trò
+CREATE TABLE IF NOT EXISTS vai_tro (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    ten_vai_tro VARCHAR(20) NOT NULL
+);
+
+-- 3. Bảng quốc gia
+CREATE TABLE IF NOT EXISTS quoc_gia (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    ten_quoc_gia VARCHAR(50) NOT NULL
+);
+
+-- 4. Bảng người dùng
+CREATE TABLE IF NOT EXISTS nguoi_dung (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    ten_dang_nhap VARCHAR(50) NOT NULL,
+    mat_khau VARCHAR(50) NOT NULL,
+    ho_ten VARCHAR(50),
+    email VARCHAR(50),
+    sdt VARCHAR(10),
+    vai_tro_id INT,
+    ngay_sinh DATETIME,
+    FOREIGN KEY (vai_tro_id) REFERENCES vai_tro(id)
+);
+
+-- 5. Bảng phim
+CREATE TABLE IF NOT EXISTS phim (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    ten_phim VARCHAR(255) NOT NULL,
+    dao_dien_id INT,
+    nam_phat_hanh INT,
+    poster VARCHAR(255),
+    quoc_gia_id INT,
+    so_tap INT,
+    trailer VARCHAR(255),
+    mo_ta TEXT,
+    FOREIGN KEY (dao_dien_id) REFERENCES nguoi_dung(id),
+    FOREIGN KEY (quoc_gia_id) REFERENCES quoc_gia(id)
+);
+
+-- 6. Bảng phim - diễn viên
+CREATE TABLE IF NOT EXISTS phim_dien_vien (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    phim_id INT,
+    dien_vien_id INT,
+    FOREIGN KEY (phim_id) REFERENCES phim(id),
+    FOREIGN KEY (dien_vien_id) REFERENCES nguoi_dung(id)
+);
+
+-- 7. Bảng phim - thể loại
+CREATE TABLE IF NOT EXISTS phim_the_loai (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    phim_id INT,
+    the_loai_id INT,
+    FOREIGN KEY (phim_id) REFERENCES phim(id),
+    FOREIGN KEY (the_loai_id) REFERENCES the_loai(id)
+);
+
+-- 8. Bảng tập phim
+CREATE TABLE IF NOT EXISTS tap_phim (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    so_tap INT,
+    tieu_de VARCHAR(255),
+    phim_id INT,
+    thoi_luong FLOAT,
+    trailer VARCHAR(255),
+    FOREIGN KEY (phim_id) REFERENCES phim(id)
+);
--- bai7/script1.js
+++ bai7/script1.js
@@ -1,32 +1,183 @@
 let danhSachFilm = [
   {
+    id: 0,
+    tenPhim: "Mua do",
+    namPhatHanh: 2025,
+    tuoi: 18,
+    thoiLuong: 1.5,
+    quocGia: "Việt Nam",
+    poster: "/2321050068/bai7/img/640x396-muado.jpg",
+    theLoai: "Phim chiếu rạp",
+    trailer: "https://youtu.be/BD6PoZJdt_M?si=XuZ5x_8BM30CNqr8",
+  },
+
+  {
     id: 1,
     tenPhim: "Beauty and Beast",
-    namPhathanh: 2025,
+    namPhatHanh: 2020,
     tuoi: 16,
     thoiLuong: 2,
-    quocGia: "Việt Nam",
+    quocGia: "Mỹ",
     poster: "/2321050068/bai7/img/phim/beautyandthebeaets.jpg",
     theLoai: "Phim chiếu rạp",
+    trailer: "https://youtu.be/ZdzBRbgNs-I?si=Qsii4jrS4PlORVwP",
   },
   {
     id: 2,
-    tenPhim: "Bông đừng trúng số",
-    namPhathanh: 2025,
+    tenPhim: "Bỗng dưng trúng số",
+    namPhatHanh: 2024,
     tuoi: 16,
     thoiLuong: 2,
     quocGia: "Việt Nam",
     poster: "/2321050068/bai7/img/phim/bongdungtrungso.jpg",
     theLoai: "Phim chiếu rạp",
+    trailer: "https://youtu.be/D3KbO3QF-lg?si=TOw47bQ7loUAxOu8",
+  },
+  {
+    id: 3,
+    tenPhim: "Cám",
+    namPhatHanh: 2025,
+    tuoi: 18,
+    thoiLuong: 2,
+    quocGia: "Việt Nam",
+    poster: "/2321050068/bai7/img/phim/cam.jpg",
+    theLoai: "Phim chiếu rạp",
+    trailer: "https://youtu.be/_8qUFEmPQbc?si=ZiIQIXbcEX6UJnvr",
+  },
+  {
+    id: 4,
+    tenPhim: "Cô giáo em là số 1",
+    namPhatHanh: 2023,
+    tuoi: 16,
+    thoiLuong: 2,
+    quocGia: "Việt Nam",
+    poster: "/2321050068/bai7/img/phim/cogiaoemlaso1.jpg",
+    theLoai: "Phim chiếu rạp",
+
+    trailer: "https://youtu.be/VPG2OG_OgL0?si=JRdTRXITMNqhOmOy",
+  },
+  {
+    id: 5,
+    tenPhim: "Conan",
+    namPhatHanh: 2025,
+    tuoi: 16,
+    thoiLuong: 2,
+    quocGia: "Nhật Bản",
+    poster: "/2321050068/bai7/img/phim/conan.jpg",
+    theLoai: "Phim chiếu rạp",
+    trailer: "https://www.youtube.com/watch?v=dz5mN-iIC4g",
+  },
+  {
+    id: 6,
+    tenPhim: "Đẹp trai thấy sai sai",
+    namPhatHanh: 2025,
+    tuoi: 18,
+    thoiLuong: 1.5,
+    quocGia: "Việt Nam",
+    poster: "/2321050068/bai7/img/phim/deptraithaysaisai.jpg",
+    theLoai: "Phim chiếu rạp",
+    trailer: "https://youtu.be/qoM5mbkOwMg?si=1P8tNoIC2n7QNhFm",
+  },
+
+  {
+    id: 7,
+    tenPhim: "Johnwick",
+    namPhatHanh: 2025,
+    tuoi: 18,
+    thoiLuong: 2,
+    quocGia: "Mỹ",
+    poster: "/2321050068/bai7/img/phim/johnwi.jpg",
+    theLoai: "Phim chiếu rạp",
+    trailer: "https://youtu.be/C0BMx-qxsP4?si=nNbpaUzaaH1TJHOl",
+  },
+  {
+    id: 8,
+    tenPhim: "Kẻ ăn hồn",
+    namPhatHanh: 2025,
+    tuoi: 16,
+    thoiLuong: 2,
+    quocGia: "Việt Nam",
+    poster: "/2321050068/bai7/img/phim/keanhon.jpg",
+    theLoai: "Phim chiếu rạp",
+    trailer: "https://youtu.be/xWh0g4rKGjI?si=FiF9WJ4lHO-E-A4Y",
+  },
+  {
+    id: 9,
+    tenPhim: "Kungfu Panda",
+    namPhatHanh: 2022,
+    tuoi: 16,
+    thoiLuong: 2,
+    quocGia: "Trung Quốc",
+    poster: "/2321050068/bai7/img/phim/kungfupanda.jpg",
+    theLoai: "Phim chiếu rạp",
+    trailer: "https://youtu.be/_inKs4eeHiI?si=9ENOWpraNOKC5G09",
+  },
+  {
+    id: 10,
+    tenPhim: "Mai",
+    namPhatHanh: 2023,
+    tuoi: 16,
+    thoiLuong: 2,
+    quocGia: "Việt Nam",
+    poster: "/2321050068/bai7/img/phim/mai.jpg",
+    theLoai: "Phim chiếu rạp",
+    trailer: "https://youtu.be/Yz96EBNwMGw?si=TRNxWOore_83HfiV",
+  },
+  {
+    id: 11,
+    tenPhim: "Mắt biếc",
+    namPhatHanh: 2018,
+    tuoi: 16,
+    thoiLuong: 2,
+    quocGia: "Việt Nam",
+    poster: "/2321050068/bai7/img/phim/matbiec.jpg",
+    theLoai: "Phim chiếu rạp",
+    trailer: "https://youtu.be/MNm77lvTfi4?si=fetjg2TjgrHtZJcn",
+  },
+  {
+    id: 12,
+    tenPhim: "Minion",
+    namPhatHanh: 2013,
+    tuoi: 16,
+    thoiLuong: 2,
+    quocGia: "Mỹ",
+    poster: "/2321050068/bai7/img/phim/minion.jpg",
+    theLoai: "Phim chiếu rạp",
+    trailer: "https://youtu.be/yDsMZn3olFw?si=rBk0x_sxHJUcF9jm",
   },
 ];
 let phimHienTai = danhSachFilm[0];
-let banner = document.getElementById("anhx");
+let banner = document.getElementById("anhto");
+let ten = document.getElementById("tenPhim");
+let nam = document.getElementById("namPhatHanh");
+let tuoi = document.getElementById("tuoi");
+let t = document.getElementById("thoiLuong");
+let q = document.getElementById("quocGia");
+let tl = document.getElementById("theLoai");
+let link = document.getElementById("linkTrailer");
+
 function chonFilm(idFilm) {
   for (let i = 0; i < danhSachFilm.length; i++) {
     if (danhSachFilm[i].id == idFilm) {
+      phimHienTai = danhSachFilm[i];
+      // alert(" Đã chọn phim: " + danhSachFilm[i].tenPhim);
       banner.src = danhSachFilm[i].poster;
-      alert("Bạn đã chọn phim: " + danhSachFilm[i].tenPhim);
+      ten.innerHTML = "Tên Film: " + danhSachFilm[i].tenPhim;
+      nam.innerHTML = "Năm PH: " + danhSachFilm[i].namPhatHanh;
+      tuoi.innerHTML = "Tuổi: " + danhSachFilm[i].tuoi + "+";
+      t.innerHTML = "Thời lượng: " + danhSachFilm[i].thoiLuong + "h";
+      q.innerHTML = "Quốc gia: " + danhSachFilm[i].quocGia;
+      tl.innerHTML = "Thể loại: " + danhSachFilm[i].theLoai;
+      return;
     }
   }
+}
+function xemTrailer() {
+  // for (let i = 0; i < danhSachFilm.length; i++) {
+  if (phimHienTai && phimHienTai.trailer) {
+    window.open(phimHienTai.trailer, "");
+  } else {
+    alert("Phim chưa có trailer.");
+  }
+  // }
 }
